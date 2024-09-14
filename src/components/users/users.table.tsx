e340f74e614face2d1cfc618b3eb91f92ab0055f
project_Html <khoanguyenduc99@gmail.com>
2024-09-14 14:15:38 +0700

    antd lib and use table

--- src/components/users/users.table.tsx
+++ src/components/users/users.table.tsx
@@ -1,8 +1,19 @@
-import React, { useEffect } from 'react'
-import '..//..//styles/users.css'
+import React, { useEffect, useState } from 'react'
+// import '..//..//styles/users.css';
+import { Table} from 'antd';
+import type { TableProps } from 'antd';
+import type, {ColumnsType} from 'antd/es/table';
 
+interface IUsers {
+    _id: string,
+    email: string,
+    name: string,
+    role: string
+}
 
 export default function UsersTable() {
+    const [listUsers, setListUsers] = useState([]);
+
 
     useEffect(() => {
         console.log(">>> check use effect");    
@@ -10,23 +21,10 @@ export default function UsersTable() {
     }, []);
     
     const getData = async () => {
-        const res = await fetch('http://localhost:8000/api/v1/auth/login', {
-            method: 'POST',
-            body: JSON.stringify({
-                username: "hoidanit@gmail.com",
-                password: "123456"
-            }),
-            headers: {
-                "Content-Type" : "application/json"
-            }
-        });
-        const data = await res.json();
+        const accessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0b2tlbiBsb2dpbiIsImlzcyI6ImZyb20gc2VydmVyIiwiX2lkIjoiNjZlMTNmNmQwY2VkZGMxYzc5ZjlmMzg0IiwiZW1haWwiOiJhZG1pbkBnbWFpbC5jb20iLCJhZGRyZXNzIjoiVmlldE5hbSIsImlzVmVyaWZ5Ijp0cnVlLCJuYW1lIjoiSSdtIGFkbWluIiwidHlwZSI6IlNZU1RFTSIsInJvbGUiOiJBRE1JTiIsImdlbmRlciI6Ik1BTEUiLCJhZ2UiOjY5LCJpYXQiOjE3MjYyOTQ0MDUsImV4cCI6MTgxMjY5NDQwNX0.sHNc79YauulOx-fk6PD4GEVxCQie8zxBMGQWr6a6IM8";
 
 
-        const accessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0b2tlbiBsb2dpbiIsImlzcyI6ImZyb20gc2VydmVyIiwiX2lkIjoiNjZlMTNmNmQwY2VkZGMxYzc5ZjlmMzg0IiwiZW1haWwiOiJhZG1pbkBnbWFpbC5jb20iLCJhZGRyZXNzIjoiVmlldE5hbSIsImlzVmVyaWZ5Ijp0cnVlLCJuYW1lIjoiSSdtIGFkbWluIiwidHlwZSI6IlNZU1RFTSIsInJvbGUiOiJBRE1JTiIsImdlbmRlciI6Ik1BTEUiLCJhZ2UiOjY5LCJpYXQiOjE3MjYxMTk4MTMsImV4cCI6MTgxMjUxOTgxM30.-CcDC_05yozRxd5f9p03cphJ-gIldwZ8RaUB37oPn2c";
-
-
-        const res1 = await fetch('http://localhost:8000/api/v1/users/all', {
+        const res = await fetch('http://localhost:8000/api/v1/users/all', {
             method: 'GET',
             
             headers: {
@@ -34,50 +32,60 @@ export default function UsersTable() {
                 "Content-Type" : "application/json"
             }
         });
-        const data1 = await res1.json();
-        console.log(">>>> check data 1: ", data1); 
+        const data = await res.json();
+        setListUsers(data.data.result);
     }
-    console.log(">>>> check render");
-  return (
+    console.log(">>>> check render list users: ", listUsers);
+
+    const columns : ColumnsType<IUsers> = [
+        {
+            title: 'Email',
+            dataIndex: 'email',
+            render: (value, record) => {
+                return(
+                    <a href="">{record.email}</a>
+                );
+            }
+        },
+        {
+            title: 'Name',
+            dataIndex: 'name'
+        },
+        {
+            title: 'Role',
+            dataIndex: 'role'
+        }
+
+    ];
+
+    return (
     <div>
-        <h2>HTML Table</h2>
-        <table>
-        <tr>
-            <th>Company</th>
-            <th>Contact</th>
-            <th>Country</th>
-        </tr>
-        <tr>
-            <td>Alfreds Futterkiste</td>
-            <td>Maria Anders</td>
-            <td>Germany</td>
-        </tr>
-        <tr>
-            <td>Centro comercial Moctezuma</td>
-            <td>Francisco Chang</td>
-            <td>Mexico</td>
-        </tr>
-        <tr>
-            <td>Ernst Handel</td>
-            <td>Roland Mendel</td>
-            <td>Austria</td>
-        </tr>
-        <tr>
-            <td>Island Trading</td>
-            <td>Helen Bennett</td>
-            <td>UK</td>
-        </tr>
-        <tr>
-            <td>Laughing Bacchus Winecellars</td>
-            <td>Yoshi Tannamuri</td>
-            <td>Canada</td>
-        </tr>
-        <tr>
-            <td>Magazzini Alimentari Riuniti</td>
-            <td>Giovanni Rovelli</td>
-            <td>Italy</td>
-        </tr>
-        </table>
+        <h2>Table Users</h2>
+        <Table columns={columns} dataSource={listUsers}/>
+      
+        {/* <table>
+                <thead>
+                    <tr>
+                        <td>Email</td>
+                        <td>Name</td>
+                        <td>Role</td>
+                    </tr>
+                </thead>
+            <tbody>
+                {
+                    listUsers.map((item: IUsers) => {
+                        return (
+                            <tr key={item._id}>
+                                <td>{item.email}</td>
+                                <td>{item.name}</td>
+                                <td>{item.role}</td>
+                            </tr>
+                        )
+                    })
+                }
+
+            </tbody>
+        </table> */}
     </div>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,69 @@
-import { StrictMode } from 'react'
+import { StrictMode, useState } from 'react'
 import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, Outlet, RouterProvider, Link } from 'react-router-dom'
 import App from './App.tsx'
 import UsersPage from './screens/users.page.tsx'
-// import './index.css'
+import { TeamOutlined, FireOutlined, SettingOutlined } from '@ant-design/icons';
+import type { MenuProps } from 'antd';
+import { Menu } from 'antd';
+import './App.scss';
+
+type MenuItem = Required<MenuProps>['items'][number];
+
+const items: MenuItem[] = [
+  {
+
+    label: <Link to={`/`}>Home</Link>,
+    key: 'home',
+    icon: <FireOutlined />,
+  },
+  {
+    label: <Link to={`/users`}> Manage Users</Link>,
+    key: 'users',
+    icon: <TeamOutlined />
+  }
+];
+
+const Header = () => {
+  const [current, setCurrent] = useState('home');
+
+  const onClick: MenuProps['onClick'] = (e) => {
+    console.log('click ', e);
+    setCurrent(e.key);
+  };
+
+  return (<Menu onClick={onClick} 
+          selectedKeys={[current]}
+          mode="horizontal" 
+          items={items} 
+        />
+    );
+};
+const LayoutAdmin = () => {
+  return(
+    <div className="">
+      <Header/>
+      <Outlet /> 
+      <footer>klhfkshjfd</footer>
+    </div>
+  )
+}
 
 const router = createBrowserRouter([{
     path: '/',
-    element: <App />
-  },
-  {
-    path: '/users',
-    element: <UsersPage />
+    // element: <App />,
+    element: <LayoutAdmin />,
+    
+    children: [
+      {
+        index: true,
+        element: <App />
+      },
+      {
+        path: 'users',
+        element: <UsersPage />
+      },
+    ]
   },
   {
     path: '/tracks',
